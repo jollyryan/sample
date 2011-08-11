5235379cd0f02036567cdf53518cc761983c374e
Ryan Northington <ryan@jollyfactory.com>
2011-08-10 20:29:44 -0500

    had to re-clone because of an install issue

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sample::Application.config.secret_token = '8444d8443b26f292b1c655edf254c7677f3bc8a0b3077b9253414eac18b7ff962533f3b8ee6aa336f4837caf15474375a79f1978e088f897b4af2dde6e8b1e80'
+Sample::Application.config.secret_token = 'aa11891aca4aaada2ba321889ca065b2af7c09c258886ca017bd52bb8ab45c13b02716e5142805caa29a9411398ab7ac1e5f5a143d8db85893d639151a6cf1fb'
